37280cd27b4e6ec8016e8be8aad93eeb11cdb5c7
Rifat <mdrifatahmed787@gmail.com>
2023-01-03 04:54:05 +0600

    update state added

--- src/Pages/MyTasks/MyTaskCard.js
+++ src/Pages/MyTasks/MyTaskCard.js
@@ -1,26 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import "./MyTaskCard.css";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-const MyTaskCard = ({
-  task,
-  tasks,
-  handleTaskDelete,
-  handleStatusUpdate,
-  handleCheckboxChange,
-}) => {
+
+const MyTaskCard = ({ task, refetch, handleTaskDelete }) => {
   const [isUpdating, setIsUpdating] = useState("");
-  const [updateTask, setUpdateTask] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
+  const [strike, setStrike] = useState(false);
 
   const navigate = useNavigate();
-
-  const handleChangeCheckbox = () => {
-    if (task.done) return null;
-
-    handleCheckboxChange(task._id);
-  };
 
   const handleDone = (id) => {
     fetch(`http://localhost:5000/donetask/${id}`, {
@@ -44,22 +32,24 @@ const MyTaskCard = ({
     const updateDoc = {
       task: task,
     };
-    // fetch(`http://localhost:5000/edittask/${updateTask}`, {
-    //   method: "PUT",
-    //   headers: {
-    //     "content-type": "application/json",
-    //   },
-    //   body: JSON.stringify(updateDoc),
-    // })
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //     setUpdateTask("");
-    //   });
+    console.log(isUpdating);
+    fetch(`http://localhost:5000/edittask/${isUpdating}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateDoc),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        refetch();
+        setIsUpdating("");
+      });
   };
 
   const renderUpdateForm = () => (
-    <form className="space-y-6 lg:px-20" action="#" onSubmit={handleUpdateTask}>
+    <form className="space-y-6 lg:px-20" onSubmit={handleUpdateTask}>
       <div className="">
         <input
           type="text"
@@ -82,38 +72,37 @@ const MyTaskCard = ({
     <div>
       <div className="max-w-lg p-6  mx-auto mt-10 bg-[#CFE8FC] border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <div className="flex justify-between">
-          <img
+          {/* <img
             className="flex items-center w-10 h-10 rounded-full"
             src={task.img}
             alt=""
-          />
-          <ul>
-            <li>
-              <div className="flex p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                <div className="flex items-center h-5">
-                  <input
-                    onClick={() => handleDone(task._id)}
-                    id="helper-radio-5"
-                    name="helper-radio"
-                    type="checkbox"
-                    checked={isChecked}
-                    value=""
-                    className="w-4 h-4 text-blue-600   focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                  />
-                </div>
-                <div className="ml-2 text-sm">
-                  <label
-                    htmlFor="helper-radio-5"
-                    className="font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    <p className={isChecked ? "line-through" : ""}>
-                      {task.task}
-                    </p>
-                  </label>
-                </div>
+          /> */}
+          <div className="flex items-center">
+            <button
+              onClick={() => handleDone(task._id) || setStrike(!strike)}
+              title="click for complete"
+              type="button"
+              className="text-gray-900 bg-white border border-red-400 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3.5 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            >
+              <Icon
+                icon="carbon:task-complete"
+                className="text-red-700"
+                width="16"
+              ></Icon>
+            </button>
+
+            <div className="pl-5">
+              <div className="focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                <p
+                  style={{
+                    textDecoration: strike === true ? "line-through" : "none",
+                  }}
+                >
+                  {task?.task}
+                </p>
               </div>
-            </li>
-          </ul>
+            </div>
+          </div>
 
           <div className="flex items-center ml-3">
             {isUpdating === task._id ? (
@@ -124,24 +113,28 @@ const MyTaskCard = ({
                   onClick={() => {
                     setIsUpdating(task._id);
                   }}
+                  title="click for edit"
                   type="button"
-                  className="text-gray-900 bg-white border border-green-500 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  className="text-gray-900 bg-white border border-green-500 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1 mr-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 >
                   <Icon
-                    icon="material-symbols:edit"
+                    icon="line-md:edit"
                     className="text-green-500"
+                    width="22"
                   ></Icon>
                 </button>
                 <button
                   onClick={() => handleTaskDelete(task._id)}
+                  title="click for delete"
                   data-modal-toggle="popup-modal"
                   id="popup-modal"
                   type="button"
-                  className="text-gray-900 bg-white border border-red-700 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  className="text-gray-900 bg-white border border-red-700 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3.5 py-1.5 mr-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 >
                   <Icon
                     icon="material-symbols:delete"
                     className="text-red-700"
+                    width="18"
                   ></Icon>
                 </button>
               </>
--- src/Pages/MyTasks/MyTasks.js
+++ src/Pages/MyTasks/MyTasks.js
@@ -1,16 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { toast } from "react-hot-toast";
-import { Icon } from "@iconify/react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllTask } from "../../components/services/actions/TaskAction";
 import { AuthContext } from "../../Context/AuthProvider";
+import Loading from "../LoadingButton/Loading";
 import MyTaskCard from "./MyTaskCard";
 
 const MyTasks = () => {
   const { user } = useContext(AuthContext);
-  const [task, setTask] = useState([]);
-  const [undoneTask, setUndoneTask] = useState([]);
 
   const {
     data: tasks = [],
@@ -22,16 +18,12 @@ const MyTasks = () => {
       const res = await fetch(
         `https://react-task-management-server.vercel.app/gettasks/${user?.email}`
       );
+
       const data = await res.json();
-      return data;
+      const undoneItem = data.filter((task) => !task.done);
+      return undoneItem;
     },
   });
-
-  //undone task showed in mytask route
-  useEffect(() => {
-    const undoneItem = tasks.filter((task) => !task.done);
-    setUndoneTask(undoneItem);
-  }, [tasks]);
 
   //task deletehandle
   const handleTaskDelete = (id) => {
@@ -44,70 +36,47 @@ const MyTasks = () => {
         .then((data) => {
           if (data.deletedCount > 0) {
             toast.success("Deleted successfully");
-            const remaining = tasks.filter((r) => r._id !== id);
-            setTask(remaining);
             refetch();
           }
         });
     }
   };
 
-  const handleCheckboxChange = (id) => {
-    const newTasks = tasks.map((task) => {
-      if (task._id === id) return { ...task, done: !task.done };
-      return task;
-    });
-    setTask(newTasks);
-  };
+  //handle checkbox
+  // const handleCheckbox = (id) => {
+  //   const newTask = tasks.map((task) => {
+  //     if (task._id === id) {
+  //       return { ...task, task: !task.done };
+  //     } else {
+  //       return task;
+  //     }
+  //   });
+  //   // settasks(newTask);
+  // };
+
   if (isLoading) {
-    return (
-      <div className="flex justify-center">
-        <button
-          disabled
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
-        >
-          <svg
-            aria-hidden="true"
-            role="status"
-            className="inline mr-3 w-4 h-4 text-white animate-spin"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="#E5E7EB"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentColor"
-            />
-          </svg>
-          Loading...
-        </button>
-      </div>
-    );
+    return <Loading></Loading>;
   }
 
   return (
     <div className="pb-16">
       <h3 className="text-2xl text-center mt-10 font-semibold text-sky-500">
-        {undoneTask.length < 2 ? (
-          <div>You have {undoneTask.length} Active Task</div>
+        {tasks.length < 2 ? (
+          <div>You have {tasks?.length} Active Task</div>
         ) : (
-          <div>You have {undoneTask.length} Active Tasks</div>
+          <div>You have {tasks?.length} Active Tasks</div>
         )}
       </h3>
-      {undoneTask &&
-        undoneTask.map((task) => (
+      {tasks &&
+        tasks?.map((task) => (
           <MyTaskCard
             key={task._id}
+            refetch={refetch}
             task={task}
             handleTaskDelete={handleTaskDelete}
-            handleCheckboxChange={handleCheckboxChange}
-            undoneTask={undoneTask}
+            // tasks={tasks}
             isLoading={isLoading}
+            // handleCheckbox={handleCheckbox}
           ></MyTaskCard>
         ))}
     </div>
